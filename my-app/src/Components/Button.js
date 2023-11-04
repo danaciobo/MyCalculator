e10480d73bc32874cbfe02adbda57f9ce9de97a9
Dana <dana_ciobo@yahoo.com>
2023-11-04 20:00:07 +0000

    working

--- my-app/src/Components/Button.js
+++ my-app/src/Components/Button.js
@@ -1,26 +1,79 @@
-export default function Buttons({ display, setDisplay, buttonValue, result, setResult, activeValue, setActiveValue }) {
+export default function Buttons({ display, setDisplay, buttonValue }) {
 
-  const operators = ['AC', '+/-', '%', '/', 'x', '-', '+', '.', '=']
+  const operators = ['AC', '+/-', '%', '/', 'x', '-', '+', '.', '='];
+
+  const handleAC = () => {
+    setDisplay('');
+  };
+
+  const handleSignChange = () => {
+    setDisplay(prevDisplay => {
+      const parts = prevDisplay.split(/([+\-/*x])/);
+
+      const lastNumber = parts.pop();
+      const sign = parts.pop();
+      if (sign === '-'){
+        const positiveNumber = parseFloat(lastNumber);
+      return [...parts, positiveNumber.toString()].join('');
+      }
+      const changedSignNumber = ((-1 * parseFloat(lastNumber))).toString();
+      return [...parts, changedSignNumber].join('');
+    });
+  };
+
+  const handleEquals = () => {
+    try {
+      const result = eval(display.replace(/x/g, '*'));
+      setDisplay(result.toString());
+    } catch (error) {
+
+      console.error(error);
+    }
+  };
+
   const handleClick = (e) => {
     e.preventDefault();
-    setActiveValue(e.target.value);
-    const nextValue = e.target.value;
-    setDisplay(nextValue);
-    operators.includes(nextValue) ? setDisplay(eval(result).toString()) : setDisplay(result.concat(nextValue));
-  }
+    let nextValue = e.target.value;
+
+    if (nextValue === 'AC'){
+      handleAC();
+    }
+    else if (nextValue === '+/-') {
+      handleSignChange();
+    }
+    else if (nextValue === '=') {
+      handleEquals();
+    }  else {
+      if (['/', 'x', '-', '+', '%'].includes(nextValue)) {
+
+        const lastChar = display.slice(-1);
+        if (['/', 'x', '-', '+', '%'].includes(lastChar)) {
+
+          setDisplay(display.slice(0, -1).concat(nextValue));
+        } else {
+
+          setDisplay(display.concat(nextValue));
+        }
+      } else {
+        setDisplay(display.concat(nextValue));
+      }
+    }
+  };
+
+  const buttonClass = operators.includes(buttonValue)
+  ? `buttons operators ${buttonValue === '=' ? 'equal' : ''}`
+  : 'buttons';
 
   return (
 
-    buttonValue === '=' ? <button type='button'
-      className='buttons operators equal'
-      value={buttonValue}
-      onClick={handleClick}>{buttonValue}</button>
-      : (operators.includes(buttonValue) ?
-        <button type='button' className='buttons operators'
-          value={buttonValue}
-          onClick={handleClick}>{buttonValue}</button> :
-        <button type='button' className='buttons' value={buttonValue}
-          onClick={handleClick}>{buttonValue}</button>)
+    <button
+    type="button"
+    className={buttonClass}
+    value={buttonValue}
+    onClick={handleClick}
+  >
+    {buttonValue}
+  </button>
 
   )
 }
